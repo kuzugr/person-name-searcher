03a75dfb8ea6a8d4b6aed34029ac0f7e88ca2bb1
kuzugr <monmon.syusyo@gmail.com>
2019-12-24 18:51:02 +0900

    first commit

---
+++ app.rb
@@ -0,0 +1,30 @@
+require 'sinatra'
+require 'natto'
+require 'pry'
+
+get '/persons' do
+  return 'Paramete Text is required' unless params['text']
+
+  natto = Natto::MeCab.new
+
+  person_names = []
+  features = []
+  person_names_index = 0
+  features_index = 0
+  natto.parse(params['text']) do |n|
+    features[features_index] = n.feature
+    features_index += 1
+    next unless n.feature.include?('人名')
+
+    if person_names == []
+      person_names[person_names_index] = n.surface
+    elsif features[features_index - 1].include?('人名')
+      person_names[person_names_index] += n.surface
+    else
+      person_names_index += 1
+      person_names[person_names_index] = n.surface
+    end
+  end
+
+  person_names.to_json
+end
